067d948f1a1c4d4574e5964cf0a26df28b36b25b
CodPatrik <54668490+CodPatrik@users.noreply.github.com>
2020-09-11 14:15:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Skapa en variabel som heter visa.
+visa kan vara antingen sant och falskt.
+
+Det ska bli två olika resultat beroende på vilket värde variabeln visa har.
+
+Om visa är sant så ska det skrivas ut följande mening i konsollen.
+"Korrekt det är sant"
+
+Om det inte är så så ska det skrivas ut följande mening 5ggr i konsollen.
+"fel"
+
+*/
+
